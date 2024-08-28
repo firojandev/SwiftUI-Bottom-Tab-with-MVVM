12ac5a083a735a5a3733c20f67388f992bcf21e3
Md Altaf Hoshain <altaf@squaregroup.com>
2024-08-28 08:24:08 +0600

    Initial Commit

---
+++ BottomTabDemo/BottomTabDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomTabDemoApp.swift
+//  BottomTabDemo
+//
+//  Created by Md Altaf Hoshain Firoj on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BottomTabDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
